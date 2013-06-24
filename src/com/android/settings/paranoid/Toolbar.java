95be0e6263dec47b272088060d103e12b01bcf78
xzzz9097 <xzzz9097@gmail.com>
2013-06-24 22:24:18 +0200

    Revert "Revert "Halo: Add master switch to disable Halo (2/2)""
    This reverts commit cebf92e065b1cb3394ca3430e7b387be2bd381b3.
    
    Conflicts:
    
    	src/com/android/settings/paranoid/Toolbar.java

--- src/com/android/settings/paranoid/Toolbar.java
+++ src/com/android/settings/paranoid/Toolbar.java
@@ -43,6 +43,7 @@ import com.android.settings.Utils;
 public class Toolbar extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
+    private static final String KEY_HALO_ENABLED = "halo_enabled";
     private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
     private static final String KEY_HALO_REVERSED = "halo_reversed";
@@ -80,6 +81,7 @@ public class Toolbar extends SettingsPreferenceFragment
     private ListPreference mPieAngle;
     private ListPreference mPieGap;
     private ListPreference mHaloState;
+    private CheckBoxPreference mHaloEnabled;
     private CheckBoxPreference mHaloHide;
     private CheckBoxPreference mHaloReversed;
     private CheckBoxPreference mHaloPause;
@@ -111,6 +113,10 @@ public class Toolbar extends SettingsPreferenceFragment
 
         mNotificationManager = INotificationManager.Stub.asInterface(
                 ServiceManager.getService(Context.NOTIFICATION_SERVICE));
+
+        mHaloEnabled = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_ENABLED);
+        mHaloEnabled.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_ENABLED, 0) == 1);
 
         mHaloState = (ListPreference) prefSet.findPreference(KEY_HALO_STATE);
         mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
@@ -288,6 +294,10 @@ public class Toolbar extends SettingsPreferenceFragment
 		} else if (preference == mFloatingShade) {	
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.FLOATING_SHADE, mFloatingShade.isChecked()
+                    ? 1 : 0);
+		} else if  (preference == mHaloEnabled) {
+             Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_ENABLED, mHaloEnabled.isChecked()
                     ? 1 : 0);	
         } else if (preference == mHaloHide) {	
             Settings.System.putInt(mContext.getContentResolver(),
